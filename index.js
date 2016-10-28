807f3e82d4cd9608aad4e5ba1a5e2689a71ff9f8
William <paep3nguin@gmail.com>
2016-10-27 19:12:37 -0500

    Generate order descriptions!

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.intent('TheUsual', {},
     function (request, response) {
         console.log("Starting usual");
         orderUsual(function (pizza) {
-            response.say("I ordered your favorite, a pizza with " + pizza).send();
+            response.say("I ordered your favorite, " + pizza).send();
         });
         return false;
     });
@@ -51,6 +51,10 @@ app.intent('OrderAPizza',
 console.log(app.schema());
 console.log(app.utterances());
 
+orderUsual(function (pizza) {
+    console.log(pizza);
+});
+
 // Export a handler function for Lambda to work with
 exports.handler = app.lambda();
 
@@ -79,8 +83,8 @@ function orderUsual(callback) {
 
     order.addItem(new pizzapi.Item({
         code: '12SCREEN',
-        options: ["B", "O"],
-        quantity: 1
+        options: ["K", "O"],
+        quantity: 21
     }));
 
     // Two medium 2-toppings for 5.99 each
@@ -93,14 +97,14 @@ function orderUsual(callback) {
         console.log("-------");
         console.log("We did it!");
         console.log(JSON.stringify(result, null, 2));
+        callback(generateOrderDescription(result.result.Order));
     });
 
-    order.price(function (result) {
-        console.log("-------");
-        console.log("Price!");
-        console.log(JSON.stringify(result, null, 2));
-        // callback(result.result.Order.Products[0].descriptions[0].value);
-    });
+    // order.price(function (result) {
+    //     console.log("-------");
+    //     console.log("Price!");
+    //     console.log(JSON.stringify(result, null, 2));
+    // });
 
     var cardNumber = '4100123422343234';
 
@@ -120,4 +124,41 @@ function orderUsual(callback) {
     //     console.log(JSON.stringify(result, null, 2));
     //     callback(result.result.Order.Products[0].descriptions[0].value);
     // });
+}
+
+function generateOrderDescription(order) {
+    var products = [];
+    for (var i = 0, l = order.Products.length; i < l; i++) {
+        var product = order.Products[i];
+        // Example Name: Medium (12") Hand Tossed Pizza
+        var name = product.Name.replace('(', '').replace(')', '').replace('"', ' inch');
+        var quantity = product.Qty;
+        if (quantity === 1) {
+            quantity = 'a';
+        } else {
+            name += 's';
+        }
+        var toppings = joinAnd(product.descriptions[0].value.split(', '));
+        products.push(quantity + ' ' + name + ' with ' + toppings);
+    }
+    return joinAnd(products);
+}
+
+// Join a bunch of strings with commas + 'and' in the right place
+// Ex. ['eggs'] -> "eggs"
+// Ex. ['eggs', 'ham'] -> "eggs and ham"
+// Ex. ['eggs', 'ham', 'bacon'] -> "eggs, ham, and bacon"
+function joinAnd(strings) {
+    if (strings.length === 1) {
+        return strings;
+    } else if (strings.length === 2) {
+        return strings[0] + ' and ' + strings[1];
+    }
+
+    var joined = "";
+    for (var i = 0, l = strings.length - 1; i < l; i++) {
+        joined += strings[i] + ', ';
+    }
+    joined += 'and ' + strings[strings.length - 1];
+    return joined;
 }
