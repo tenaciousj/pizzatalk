bac33a977e2f74aae30ff2326a341d0eb9325d2e
paep3nguin <paep3nguin@gmail.com>
2016-11-02 22:36:26 -0500

    Much better builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,49 +13,18 @@ module.exports = function(grunt) {
         src: "dist.zip"
       }
     },
-    compress: {
-      dist: {
+    shell: {
         options: {
-          archive: "dist.zip",
-          mode: "zip"
+            stderr: false
         },
-        files: [{
-          expand: true,
-          src: [
-              'index.js',
-              'node_modules/**'
-            ]
-        }]
-      }
-    },
-    rename: {
-      start1: {
-        files: [{
-          src: ['node_modules'],
-          dest: ['fake_node_modules']
-        }]
-      },
-      start2: {
-        files: [{
-          src: ['real_node_modules'],
-          dest: ['node_modules']
-        }]
-      },
-      end1: {
-          src: ['node_modules'],
-          dest: ['real_node_modules']
-      },
-      end2: {
-          src: ['fake_node_modules'],
-          dest: ['node_modules']
-      }
+        dist: 'jar uvf dist.zip index.js'
     }
   });
 
   grunt.loadNpmTasks('grunt-aws');
   grunt.loadNpmTasks('grunt-beep');
-  grunt.loadNpmTasks('grunt-contrib-rename');
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-compress');
 
-  grunt.registerTask('default', ['rename:start1', 'rename:start2', 'compress:dist', 's3:dist', 'beep:2', 'rename:end1', 'rename:end2']);
+  grunt.registerTask('default', ['shell:dist', 's3:dist', 'beep:2']);
 };
